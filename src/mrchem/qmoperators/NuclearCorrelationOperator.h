2657174def35663559785cd6b339c5bb5b8d7295
Stig Rune Jensen <stig.r.jensen@uit.no>
2017-09-04 09:39:23 +0200

    extend to multiple nuclei
    Seems to work, but accuracy/convergence needs to be properly tested

--- src/mrchem/qmoperators/NuclearCorrelationOperator.h
+++ src/mrchem/qmoperators/NuclearCorrelationOperator.h
@@ -1,6 +1,8 @@
 #ifndef NUCLEARCORRELATIONOPERATOR_H
 #define NUCLEARCORRELATIONOPERATOR_H
 
+#include <vector>
+
 #include "AnalyticPotential.h"
 #include "NuclearCorrelationFunction.h"
 #include "Nucleus.h"
@@ -9,8 +11,19 @@ class NuclearCorrelationOperator : public AnalyticPotential {
 public:
     NuclearCorrelationOperator(const Nuclei &nucs,
                                const NuclearCorrelationFunction &S) {
-        const Nucleus &nuc = nucs[0];
-        setReal(S.getS_0(nuc));
+        std::vector<std::function<double (const double *r)> > funcs;
+        for (int k = 0; k < nucs.size(); k++) {
+            funcs.push_back(S.getS_0(nucs[k]));
+        }
+
+        auto f = [funcs] (const double *r) -> double {
+            double result = 1.0;
+            for (int i = 0; i < funcs.size(); i++) {
+                result *= funcs[i](r);
+            }
+            return result;
+        };
+        setReal(f);
     }
     virtual ~NuclearCorrelationOperator() {
     }
--- src/mrchem/qmoperators/RegularizedPotential.h
+++ src/mrchem/qmoperators/RegularizedPotential.h
@@ -10,14 +10,13 @@
 class RegularizedPotential : public RankZeroTensorOperator {
 public:
     RegularizedPotential(Nuclei &nucs,
-                         NuclearCorrelationFunction &R,
+                         NuclearCorrelationFunction &S,
                          DerivativeOperator<3> &D)
             : nuclei(nucs), nabla(D) {
-        const Nucleus &nuc = this->nuclei[0];
-        U_x.setReal(R.getS_1(nuc, 0));
-        U_y.setReal(R.getS_1(nuc, 1));
-        U_z.setReal(R.getS_1(nuc, 2));
-        U_2.setReal(R.getS_2(nuc));
+        setupU_1(nucs, S, 0);
+        setupU_1(nucs, S, 1);
+        setupU_1(nucs, S, 2);
+        setupU_2(nucs, S);
         initializeTensorOperator();
     }
     virtual ~RegularizedPotential() { }
@@ -28,17 +27,46 @@ public:
 protected:
     Nuclei nuclei;
     NablaOperator nabla;
-    AnalyticPotential U_x;
-    AnalyticPotential U_y;
-    AnalyticPotential U_z;
+    AnalyticPotential U_1[3];
     AnalyticPotential U_2;
 
     void initializeTensorOperator() {
         RankZeroTensorOperator &h = (*this);
-        h = -1.0*(U_x*nabla[0] +
-                  U_y*nabla[1] +
-                  U_z*nabla[2] +
+        h = -1.0*(U_1[0]*nabla[0] +
+                  U_1[1]*nabla[1] +
+                  U_1[2]*nabla[2] +
                   U_2);
+    }
+
+    void setupU_1(const Nuclei &nucs, NuclearCorrelationFunction &S, int d) {
+        auto f = [nucs, &S, d] (const double *r) -> double {
+            double result = 0.0;
+            for (int i = 0; i < nucs.size(); i++) {
+                result += S.getS_1(nucs[i], d)(r);
+            }
+            return result;
+        };
+
+        this->U_1[d].setReal(f);
+    }
+
+    void setupU_2(const Nuclei &nucs, NuclearCorrelationFunction &S) {
+        auto f = [nucs, &S] (const double *r) -> double {
+            double result = 0.0;
+            for (int a = 0; a < nucs.size(); a++) {
+                const Nucleus &A = nucs[a];
+                for (int b = 0; b < a; b++) {
+                    const Nucleus &B = nucs[b];
+                    result += S.getS_1(A, 0)(r)*S.getS_1(B, 0)(r);
+                    result += S.getS_1(A, 1)(r)*S.getS_1(B, 1)(r);
+                    result += S.getS_1(A, 2)(r)*S.getS_1(B, 2)(r);
+                }
+                result += S.getS_2(A)(r);
+            }
+            return result;
+        };
+
+        this->U_2.setReal(f);
     }
 };
 
