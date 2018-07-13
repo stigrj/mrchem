994111dd8de24c0ef3991e9fb57569160132c569
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-07-13 13:32:21 +0200

    Use AnalyticPotential in PositionOperator

--- src/qmoperators/one_electron/PositionOperator.h
+++ src/qmoperators/one_electron/PositionOperator.h
@@ -1,39 +1,35 @@
 #pragma once
 
-#include "QMPotential.h"
 #include "RankOneTensorOperator.h"
+#include "AnalyticPotential.h"
 
 namespace mrchem {
 
-class PositionPotential final : public QMPotential {
-public:
-    PositionPotential(int d, const double *o);
-    ~PositionPotential() { }
-
-protected:
-    mrcpp::AnalyticFunction<3> func;
-
-    void setup(double prec);
-    void clear();
-};
-
 class PositionOperator : public RankOneTensorOperator<3> {
 public:
-    PositionOperator(const double *o = 0)
-            : r_x(0, o),
-              r_y(1, o),
-              r_z(2, o) {
+    PositionOperator(const double *o = 0) {
+        double o_x = (o != 0) ? o[0] : 0.0;
+        double o_y = (o != 0) ? o[1] : 0.0;
+        double o_z = (o != 0) ? o[2] : 0.0;
+
+        auto f_x = [o_x] (const double *r) -> double { return r[0] - o_x; };
+        auto f_y = [o_y] (const double *r) -> double { return r[1] - o_y; };
+        auto f_z = [o_z] (const double *r) -> double { return r[2] - o_z; };
+
+        this->r_x.setReal(f_x);
+        this->r_y.setReal(f_y);
+        this->r_z.setReal(f_z);
+
         RankOneTensorOperator &r = (*this);
         r[0] = r_x;
         r[1] = r_y;
         r[2] = r_z;
     }
-    virtual ~PositionOperator() { }
 
 protected:
-    PositionPotential r_x;
-    PositionPotential r_y;
-    PositionPotential r_z;
+    AnalyticPotential r_x;
+    AnalyticPotential r_y;
+    AnalyticPotential r_z;
 };
 
 } //namespace mrchem
