03c39e6864f6ace9a1a13be5015b931cb09cc05e
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-07-13 13:47:57 +0200

    Use AnalyticPotential in NuclearCorrelationOperator

--- src/qmoperators/one_electron/NuclearCorrelationOperator.h
+++ src/qmoperators/one_electron/NuclearCorrelationOperator.h
@@ -1,32 +1,34 @@
 #pragma once
 
-#include "QMPotential.h"
 #include "RankZeroTensorOperator.h"
+#include "AnalyticPotential.h"
+#include "NuclearCorrelationFunction.h"
 
 namespace mrchem {
-class NuclearCorrelationFunction;
-
-class NuclearCorrelationPotential final : public QMPotential {
-public:
-    NuclearCorrelationPotential(const Nuclei &nucs, const NuclearCorrelationFunction &S);
-
-protected:
-    mrcpp::AnalyticFunction<3> func;
-
-    void setup(double prec);
-    void clear();
-};
 
 class NuclearCorrelationOperator final : public RankZeroTensorOperator {
 public:
-    NuclearCorrelationOperator(const Nuclei &nucs, const NuclearCorrelationFunction &S)
-            : ncp(nucs, S) {
+    NuclearCorrelationOperator(const Nuclei &nucs, const NuclearCorrelationFunction &S) {
+        std::vector<DoubleFunction> funcs;
+        for (int k = 0; k < nucs.size(); k++) {
+            funcs.push_back(S.getS_0(nucs[k]));
+        }
+
+        auto f = [funcs] (const double *r) -> double {
+            double result = 1.0;
+            for (int i = 0; i < funcs.size(); i++) {
+                result *= funcs[i](r);
+            }
+            return result;
+        };
+        this->ncp.setReal(f);
+
         RankZeroTensorOperator &R = (*this);
         R = ncp;
     }
 
 protected:
-    NuclearCorrelationPotential ncp;
+    AnalyticPotential ncp;
 };
 
 } //namespace mrchem
