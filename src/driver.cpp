1280e82362e4bd4062135ba206d2931571e7a107
Anders Brakestad <anders.brakestad>
2021-11-18 20:37:20 +0100

    Load one minus kappe potential into FockOperator

--- src/driver.cpp
+++ src/driver.cpp
@@ -982,11 +982,14 @@ void driver::build_fock_operator(const json &json_fock, Molecule &mol, FockOpera
         std::shared_ptr<RankZeroOperator> mod_Z_p = Z_p->divKappaOverSqKappa();
         auto scaled_Z_p = std::make_shared<RankZeroOperator>(Z_p->zFacVz());
         auto inv_Z_p = std::make_shared<RankZeroOperator>(Z_p->invKappa());
+        auto omk = std::make_shared<RankZeroOperator>(Z_p->oneMinusKappa());
+        
         F.getZoraOperator() = Z_p;
         F.getSqrtZora() = sqrt_Z_p;
         F.getModZora() = mod_Z_p;
         F.getScaledZora() = scaled_Z_p;
         F.getInvZora() = inv_Z_p;
+        F.getOneMinusZoraPot() = omk;
 
         F.zoraKineticAlgorithm = algo_kinetic;
         F.zoraTakeAlgorithm = algo_take;
