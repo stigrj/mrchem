0b900c529db8f01d9417a629920373c3d7bc3832
Stig Rune Jensen <stig.r.jensen@uit.no>
2022-02-02 17:14:40 +0100

    Add alpha and beta spin operators

--- src/qmoperators/QMSpin.cpp
+++ src/qmoperators/QMSpin.cpp
@@ -42,6 +42,10 @@ using QMOperator_p = std::shared_ptr<mrchem::QMOperator>;
 
 namespace mrchem {
 
+///////////////////////////////////////////////
+//////////          QMSpin           //////////
+///////////////////////////////////////////////
+
 Orbital QMSpin::apply(Orbital inp) {
     if (this->apply_prec < 0.0) MSG_ERROR("Uninitialized operator");
 
@@ -89,6 +93,86 @@ QMOperatorVector QMSpin::apply(QMOperator_p &O) {
         // fallback: treat as individual operators
         out.push_back(O);
         out.push_back(std::make_shared<QMSpin>(*this));
+    }
+
+    return out;
+}
+
+///////////////////////////////////////////////
+//////////          QMAlpha          //////////
+///////////////////////////////////////////////
+
+/** @brief Identity on alpha orbital, zero on beta orbital */
+Orbital QMAlpha::apply(Orbital inp) {
+    if (this->apply_prec < 0.0) MSG_ERROR("Uninitialized operator");
+
+    Orbital out(SPIN::Alpha, 1, inp.rankID());
+    if (inp.spin() == SPIN::Alpha || inp.spin() == SPIN::Paired) qmfunction::deep_copy(out, inp);
+    return out;
+}
+
+/** @brief Identity on alpha orbital, zero on beta orbital */
+Orbital QMAlpha::dagger(Orbital inp) {
+    if (this->apply_prec < 0.0) MSG_ERROR("Uninitialized operator");
+
+    Orbital out(SPIN::Alpha, 1, inp.rankID());
+    if (inp.spin() == SPIN::Alpha || inp.spin() == SPIN::Paired) qmfunction::deep_copy(out, inp);
+    return out;
+}
+
+QMOperatorVector QMAlpha::apply(QMOperator_p &O) {
+    QMIdentity *I = dynamic_cast<QMIdentity *>(&(*O));
+    QMAlpha *A = dynamic_cast<QMAlpha *>(&(*O));
+
+    QMOperatorVector out;
+    if (I or A) {
+        // O == identity: skip it
+        // O == alpha: alpha^2 == alpha -> skip it
+        out.push_back(std::make_shared<QMAlpha>(*this));
+    } else {
+        // fallback: treat as individual operators
+        out.push_back(O);
+        out.push_back(std::make_shared<QMAlpha>(*this));
+    }
+
+    return out;
+}
+
+///////////////////////////////////////////////
+//////////          QMBeta           //////////
+///////////////////////////////////////////////
+
+/** @brief Identity on beta orbital, zero on alpha orbital */
+Orbital QMBeta::apply(Orbital inp) {
+    if (this->apply_prec < 0.0) MSG_ERROR("Uninitialized operator");
+
+    Orbital out(SPIN::Beta, 1, inp.rankID());
+    if (inp.spin() == SPIN::Beta || inp.spin() == SPIN::Paired) qmfunction::deep_copy(out, inp);
+    return out;
+}
+
+/** @brief Identity on beta orbital, zero on alpha orbital */
+Orbital QMBeta::dagger(Orbital inp) {
+    if (this->apply_prec < 0.0) MSG_ERROR("Uninitialized operator");
+
+    Orbital out(SPIN::Beta, 1, inp.rankID());
+    if (inp.spin() == SPIN::Beta || inp.spin() == SPIN::Paired) qmfunction::deep_copy(out, inp);
+    return out;
+}
+
+QMOperatorVector QMBeta::apply(QMOperator_p &O) {
+    QMOperatorVector out;
+
+    QMIdentity *I = dynamic_cast<QMIdentity *>(&(*O));
+    QMBeta *B = dynamic_cast<QMBeta *>(&(*O));
+    if (I or B) {
+        // O == identity: skip it
+        // O == beta: beta^2 == beta -> skip it
+        out.push_back(std::make_shared<QMBeta>(*this));
+    } else {
+        // fallback: treat as individual operators
+        out.push_back(O);
+        out.push_back(std::make_shared<QMBeta>(*this));
     }
 
     return out;
